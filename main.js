03051484d9bf9048cd85221d52d46ece16e4c5a1
Henrique <henriquehsf2014@gmail.com>
2022-11-04 09:48:37 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function creatGame(player1, hour, player2) {
+  return `
+    <li>
+      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+      <strong>${hour}</strong>
+      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+    </li>
+  `
+}
+
+function creatCard(date, day, games) {
+  return `
+    <div class="card">
+      <h2>${date} <span>${day}</span></h2>
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `
+}
+document.querySelector("#cards").innerHTML = 
+      creatCard("24/11", "quinta", creatGame("Brasil", "16:00", "Servia")) +
+      creatCard("28/11", "segunda", creatGame("Brasil", "13:00", "Suiça")) +
+      creatCard("02/12", "sexta", creatGame("Brasil", "16:00", "Camaroes"))
+
